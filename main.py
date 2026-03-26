0e7e07c5e35b017a583a4f46d9e39fbde8780c5d
Morbanaa <theodorerodd@gmail.com>
2026-03-25 19:40:56 -0500

    Added comments to code

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import os
 import platform
 
 def main():
-    number = create_number()
+    number = create_number() # Creates random number
+    clear_screan() # Cleans up the console
+    
+    # Runs while the random number and user guess do not match...
     while True:
         guess = get_user_input()
         win = check_match(guess,number)
@@ -14,19 +17,19 @@ def main():
             print(f"You Win! The number was {number}")
             break
         else:
-            print("Try again")
+            print("Try again you")
 
-
+# Creates random number 1-6
 def create_number():
     number = random.randint(1,6)
     return number
 
-
+# Asks user to guess a number
 def get_user_input():
     guess = int(input("What number am I thinking off? "))
     return guess
 
-
+# Checks win state
 def check_match(guess,number):
     if guess == number:
         win = True
@@ -35,14 +38,14 @@ def check_match(guess,number):
         win = False
         return win
 
-
+# Console formating
 def clear_screan():
     if platform.system() == "Windows":
         os.system("cls")
     else:
         os.system("clear")
 
-
+# Program Entry point
 if __name__ == "__main__":
     main()
     print("Have a great day!\n")
