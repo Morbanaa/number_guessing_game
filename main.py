615ca4b6729800a85c55861520a257520937c4e0
Morbanaa <theodorerodd@gmail.com>
2026-03-25 19:01:02 -0500

    Initial Commit for main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# Teddy Rodd
+# Number Guessing Game
+
+import random
+import os
+import platform
+
+def main():
+    print("Hello world")
+    clear_screan()
+
+
+def create_numbers():
+    pass
+
+
+def get_user_input():
+    pass
+
+
+def check_match():
+    pass
+
+
+def clear_screan():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+if __name__ == "__main__":
+    main()
+    print("Have a great day!\n")
